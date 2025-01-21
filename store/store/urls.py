0006b281d4d196c321d35630acbc7dc3de93b436
Dima1385 <Lozik1385@gmail.com>
2025-01-22 01:31:34 +0200

    update
    Design update

--- store/store/urls.py
+++ store/store/urls.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
+    path('product/<int:product_id>/', views.product_detail, name='product_detail'),
     path('register/', views.register, name='register'),
     path('login/', views.login_view, name='login'),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
